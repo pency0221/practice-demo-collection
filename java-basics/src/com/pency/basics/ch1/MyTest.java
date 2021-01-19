46309a3d86c94bd2e0e1afda8134b1297c8d5010
Pency <wangpengcheng1@chinaport.gov.cn>
2021-01-19 17:53:57 +0800

    2:流程控制 只梳理了没怎么用过的

--- java-basics/src/com/pency/basics/ch1/MyTest.java
+++ java-basics/src/com/pency/basics/ch1/MyTest.java
@@ -5,18 +5,18 @@ import org.junit.Test;
 import java.sql.SQLOutput;
 
 /**
- * @Author Pency
- * @Description 数据类型 隐式类型转换、显式类型转换  及注意点
- *     总结：
- *        1、因为int和double是整型和浮点型的默认类型  所以定义long 和 float要加L(尽量大写 防止视觉和1混淆)和f后缀显式说明    double尽量也加d（可读性）
- *        2、对JVM看来，short,byte,int都是同一样的东西  所以虽然不是默认类型定义byte/short/char时也不用加后缀 根据int字面值得大小能存下就直接（转成需要的类型）存了  存不下当场报错
- *        3、当byte、short、char三者在计算后的结果都转换为了int类型（包含同类型之间的计算 例如byte+byte 最终结果也是int）
- *        4、==比较引用（有的话）  equals比较值   前提是自定义的类equals需要重写（object中定义 equals返回==的值）才能实现比较值  jdk部分类已经重写 比如String
- **/
+ * TODO 类型转换  后缀问题  计算转换问题
+ */
 public class MyTest {
     /**
-     * TODO 类型转换  后缀问题  计算转换问题
-     */
+     * @Author Pency
+     * @Description 数据类型 隐式类型转换、显式类型转换  及注意点
+     *     总结：
+     *        1、因为int和double是整型和浮点型的默认类型  所以定义long 和 float要加L(尽量大写 防止视觉和1混淆)和f后缀显式说明    double尽量也加d（可读性）
+     *        2、对JVM看来，short,byte,int都是同一样的东西  所以虽然不是默认类型定义byte/short/char时也不用加后缀 根据int字面值得大小能存下就直接（转成需要的类型）存了  存不下当场报错
+     *        3、当byte、short、char三者在计算后的结果都转换为了int类型（包含同类型之间的计算 例如byte+byte 最终结果也是int）
+     *        4、==比较引用（有的话）  equals比较值   前提是自定义的类equals需要重写（object中定义 equals返回==的值）才能实现比较值  jdk部分类已经重写 比如String
+     **/
     @Test
     public void test1(){
         //TODO pency:Java中为什么给float类型变量赋值需要加F,而给byte、short赋值的时候却不需要呢？
@@ -89,5 +89,32 @@ public class MyTest {
         //ps:== 和 equals  ==比较引用（有的话） equals比较值(只能说对象设计目的上 本省定义在object实现还是调用的== jdk中的类有比较值相同特征的类大部分都重写了equals方法来实现比较值得目的
         // 我们自己定义的类想要比较值相同则需要自己重写equals)  基本数据类型不是类没有equals 比较大小只能用==
         //且基本数据类型在只在栈区开辟空间 存的是值 所以==此时比较的是就是值（可以把==直接理解成比较的是栈区存的内容 存的是值就比较值 对象时存的是引用地址 所以就比较引用）
+    }
+
+
+    /**
+     * 流程控制 只梳理没怎么用过的。。。
+     * swich case
+     */
+    @Test
+    public void test2(){
+        int i=2;
+        switch (3){
+            case 0:
+                System.out.println("A");
+            case 1:
+                System.out.println("B");
+                break;
+            //case "sdafds":    //case 值得类型要和switch类型相同 现在i是int  这里就不能是String
+            case 3:
+                System.out.println("C");
+            default:
+                System.out.println("D");
+        }
+        //结果：
+        // i=0时  A B
+        // i=1时  B
+        // i=2时  D
+        // i=3时  C D
     }
 }
